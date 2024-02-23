3d0f4b6fc641500e811fb74916cfaf56e07134a6
yatharthsameer <ytharthsmr@gmail.com>
2024-02-24 04:08:38 +0530

    test changes to server

--- Backend/server.py
+++ Backend/server.py
@@ -302,5 +302,9 @@ def search_embed():
 
 
 if __name__ == "__main__":
-    print("Starting the server...")
-    app.run(debug=True, port=3001)
+    # This is used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app. You
+    # can configure startup instructions by adding `entrypoint` to app.yaml.
+    app.run(host="127.0.0.1", port=8080, debug=True)
+# [END gae_python3_app]
+# [END gae_python38_app]
