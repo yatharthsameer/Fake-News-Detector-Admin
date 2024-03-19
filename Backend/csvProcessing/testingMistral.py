2b31309037694ab83950709f4d95c11a64b44fcf
yatharthsameer <ytharthsmr@gmail.com>
2024-03-19 18:26:52 +0530

    trying mistral again

--- Backend/csvProcessing/testingMistral.py
+++ Backend/csvProcessing/testingMistral.py
@@ -1,11 +1,8 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
-device = "cuda"  # the device to load the model onto
 
-model = AutoModelForCausalLM.from_pretrained(
-    "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
-)
-tokenizer = AutoTokenizer.from_pretrained("TinyLlama/TinyLlama-1.1B-Chat-v1.0")
+model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-Instruct-v0.2")
+tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-Instruct-v0.2")
 
 messages = [
     {"role": "user", "content": "What is your favourite condiment?"},
@@ -18,8 +15,7 @@ messages = [
 
 encodeds = tokenizer.apply_chat_template(messages, return_tensors="pt")
 
-model_inputs = encodeds # .to(device)
-# model.to(device)
+model_inputs = encodeds 
 
 generated_ids = model.generate(model_inputs, max_new_tokens=1000, do_sample=True)
 decoded = tokenizer.batch_decode(generated_ids)
