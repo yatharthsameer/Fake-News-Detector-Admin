d8e1adc9cd8ef2ddbc86a6fab3741c568874c491
yatharthsameer <ytharthsmr@gmail.com>
2024-03-19 18:24:26 +0530

    trying tinyllm

--- Backend/csvProcessing/testingMistral.py
+++ Backend/csvProcessing/testingMistral.py
@@ -1,24 +1,12 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
-import torch
 
-# Set the model path
-MODEL_PATH = "mistralai/Mistral-7B-Instruct-v0.2"
+device = "cuda"  # the device to load the model onto
 
-# Initialize the tokenizer
-tokenizer = AutoTokenizer.from_pretrained(MODEL_PATH)
+model = AutoModelForCausalLM.from_pretrained(
+    "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
+)
+tokenizer = AutoTokenizer.from_pretrained("TinyLlama/TinyLlama-1.1B-Chat-v1.0")
 
-# Initialize the model and convert it to use bfloat16 for memory efficiency
-model = AutoModelForCausalLM.from_pretrained(MODEL_PATH).to(torch.bfloat16())
-
-# Adjust tokenizer padding and pad token settings
-tokenizer.padding_side = "left"
-# Use EOS token as pad token if a specific pad token is not set
-tokenizer.pad_token_id = tokenizer.eos_token_id
-tokenizer.pad_token = tokenizer.eos_token
-# Ensure the model's pad token id is set correctly
-model.config.pad_token_id = tokenizer.eos_token_id
-
-# Example conversation/messages
 messages = [
     {"role": "user", "content": "What is your favourite condiment?"},
     {
@@ -28,17 +16,11 @@ messages = [
     {"role": "user", "content": "Do you have mayonnaise recipes?"},
 ]
 
-# Tokenize and format the conversation using the chat history template
-encoded_inputs = tokenizer(messages, return_tensors="pt", padding=True)
+encodeds = tokenizer.apply_chat_template(messages, return_tensors="pt")
 
-# Move the model inputs to the CPU
-model_inputs = encoded_inputs.to("cpu")
+model_inputs = encodeds # .to(device)
+# model.to(device)
 
-# Generate a response from the model
-generated_ids = model.generate(**model_inputs, max_new_tokens=1000, do_sample=True)
-
-# Decode the generated ids to get the text
-decoded = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)
-
-# Print the generated text
+generated_ids = model.generate(model_inputs, max_new_tokens=1000, do_sample=True)
+decoded = tokenizer.batch_decode(generated_ids)
 print(decoded[0])
