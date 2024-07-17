b266ff26f3d1ed52d662a0400ba8688aba41e5ee
yatharthsameer <ytharthsmr@gmail.com>
2024-07-18 01:37:29 +0530

    removed 7 am scheduler

--- Backend/scheduler.py
+++ Backend/scheduler.py
@@ -5,9 +5,9 @@ from background_tasks import fetch_and_store_top_trends
 
 def run_scheduler():
     print("Starting scheduler...")
-    # Schedule the task to run at 11 AM and 6 PM every day
-    schedule.every().day.at("7:00").do(fetch_and_store_top_trends)
-    print("Scheduled 7:00")
+    # # Schedule the task to run at 11 AM and 6 PM every day
+    # schedule.every().day.at("7:00").do(fetch_and_store_top_trends)
+    # print("Scheduled 7:00")
     schedule.every().day.at("11:00").do(fetch_and_store_top_trends)
     print("Scheduled 11:00")
     schedule.every().day.at("12:00").do(fetch_and_store_top_trends)
